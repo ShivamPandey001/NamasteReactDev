dd3cd58fbb071a2c22908a896d02c673460ad1f8
ShivamPandey001 <cse.shivam98@gmail.com>
2022-12-06 23:36:08 +0530

    facing some issue on interpolating the data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const title = "HungerHub";
+const t = "Hunger Hub";
 
 const HeadingComponent = () => {
-    return (
             <div id="title" className='=title-class'>
-            <h2>{title}</h2>
+            <h2>Hunger Hub</h2>
             </div>
-        );
 };
-
-const CardComponent = () => {
+const data = [
+{
+    img : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/uj68ybtfwbydewraxzhm",
+    name : "GayLord Express",
+    cusine: "Agra , India",
+    star : 4
+},
+{
+    img : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/uj68ybtfwbydewraxzhm",
+    name : "Day Dreams Cafe and Juicebar",
+    cusine: "Beverages, Fast Food, Italian, Ice Cream",
+    star : 4
+},
+{
+    img : "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/wcd8jyciflmzxm3yooei",
+    name : "Biryani station",
+    cusine: "Indian, Biryani, North Indian, Punjabi",
+    star : 3
+}
+];
+const CardComponent = (props) => {
     return (
         <div id="card" className="card">
-
+            <img src="props.restuarant.img"></img>
+            <h2> {props.restuarant.name} </h2>
+            <h3> {props.restuarant.cusine} </h3>
+            <h4>{props.restuarant.star} star</h4>
         </div>
     );
-
 }
+// const  props ={
+// restuarant = data[0];  it's like this interally
+// }
+const BodyComponent = () => (
+<div className='card-container'>
+    <CardComponent restuarant ={data[0]}/>
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+    <CardComponent />
+</div>
+);
+
+const AppLayout = () => (
+    <>
+    <HeadingComponent />
+    <BodyComponent />
+    </>
+)
 console.log(<HeadingComponent/>);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
